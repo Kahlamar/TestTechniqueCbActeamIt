8c0f62824bc8b44a0de70d4d36056b4a9aefc6de
Cyril Brun <brun.cyril@hotmail.fr>
2025-09-25 16:40:00 +0200

    Init View

---
+++ TestTechniqueCbActeamIt/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1>Panneau d'information de gare</h1>
+    <div class="tables">
+      <div>
+        <h2>Arrivées</h2>
+        <table id="tableau-arrivees">
+          <thead>
+            <tr>
+              <th>Numéro</th>
+              <th>Provenance</th>
+              <th>Heure</th>
+              <th>Retard</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+
+      <div>
+        <h2>Départs</h2>
+        <table id="tableau-departs">
+          <thead>
+            <tr>
+              <th>Numéro</th>
+              <th>Destination</th>
+              <th>Heure</th>
+              <th>Retard</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
